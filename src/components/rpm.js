29df8774b1d51fa6463f8bf28bec23cc9d2322e0
Andrew Mirsky <andrew@mirsky.net>
2021-03-19 21:56:30 -0400

    react speedo

--- src/components/rpm.js
+++ src/components/rpm.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import {Box, Meter, Stack, Text} from "grommet";
+import ReactSpeedometer from "react-d3-speedometer"
 
 
 export const RPMGauge = () => {
 
   const rpm = useSelector(state => state.ecu.rpm);
 
-  const meterValue = 20;
+  let options = {
+    minValue: 0,
+    maxValue: 8000,
+    segments: 1,
+    width:175,
+    startColor: "#007700",
+    endColor: "#00FF00",
+    ringWidth: 20,
+    currentValueText: "${value} rpm"
+  }
 
   return (
     <Box align="center" pad="xsmall">
     <Stack anchor="center">
-      <Meter
-        type="circle"
-        background="light-2"
-        values={[{ value: 25 }, {value:70}, {value: 25}]}
-        size="small"
-        thickness="medium"
-      />
-      <Meter
-        type="circle"
-        background="light-3"
-        values={[{ value: 25, label: "psi" }]}
-        size="xsmall"
-        thickness="medium"
-
-      />
+      <ReactSpeedometer {...options} />
     </Stack>
     </Box>
   )
